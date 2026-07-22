ab5b5f9025f339d36e39d83cf6d3d6e65845236c
Hussain Lodhi <hasanlodhi000@gmail.com>
2026-07-22 22:57:30 +0500

    Initial commit for Demo Frontend

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
+
+app.options('*', cors());
+
 app.use(express.json());
 
 // ----------------------------------------------------------------------------

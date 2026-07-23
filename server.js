b3bd99339c30bb1a89940f6015011129d90f9a11
Hussain Lodhi <hasanlodhi000@gmail.com>
2026-07-23 20:02:15 +0500

    ..

--- server.js
+++ server.js
@@ -20,9 +20,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
-
 app.options('*', cors());
-
 app.use(express.json());
 
 // ----------------------------------------------------------------------------
